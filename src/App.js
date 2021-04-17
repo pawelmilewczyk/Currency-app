32d5307442805239e15f5890dec39d6aebcf646e
pawelmilewczyk <milewczykp@gmail.com>
2021-04-17 09:04:44 +0200

    Add condition to render Favorites in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.scss";
 import Form from "./components/Form/Form";
 import Favorites from "./components/Favorites/Favorites";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <Form />
-      <Favorites />
+      {props.favorites.length > 0 ? <Favorites /> : null}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    favorites: state.favoriteCurrencies,
+  };
+};
+
+export default connect(mapStateToProps)(App);
