c42f72d55848a30a12a4d246c5d1cabb7ec606b5
pawelmilewczyk <milewczykp@gmail.com>
2021-04-17 08:53:55 +0200

    Style Favorites component

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,7 +1,15 @@
 import "./Favorites.scss";
 
 const Favorites = (props) => {
-  return <div>Favorites</div>;
+  return (
+    <div className="Favorites">
+      <div className="Titles grid">
+        <h5>Currency</h5>
+        <h5>Bid</h5>
+        <h5>Ask</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Favorites;
