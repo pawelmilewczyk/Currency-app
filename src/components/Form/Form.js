17821f994451b6e7dd1cfcead005b2703b1a7a1b
pawelmilewczyk <milewczykp@gmail.com>
2021-04-17 07:17:08 +0200

    Style Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,10 +3,12 @@ import Button from "../../shared/Button/Button";
 
 const Form = (props) => {
   return (
-    <form>
-      <h1>Choose your favorites currencies</h1>
+    <form className="Form">
+      <h1 className="Title">Choose your favorites currencies</h1>
       <select>
         <option>SELECT</option>
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
       </select>
       <Button type={"Submit"}>Add</Button>
       <Button type={"Remove"}>Remove all</Button>
