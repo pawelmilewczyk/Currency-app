fd1ce3a67089ea6619d698b4ad9f7df7d5bdb2fe
pawelmilewczyk <milewczykp@gmail.com>
2021-04-17 09:52:55 +0200

    Update state after removing favorite currency

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,17 +1,53 @@
 import "./Popup.scss";
 import Button from "../../shared/Button/Button";
+import * as action from "../../store/actions";
+import { connect } from "react-redux";
 
 const Popup = (props) => {
+  const removeHandler = () => {
+    console.log(props.currencyToRemove);
+    props.updateFavorites(
+      props.favorites.filter(
+        (favorite) => favorite.code !== props.currencyToRemove
+      )
+    );
+
+    props.updateCurrencies(
+      props.favorites
+        .filter((favorite) => favorite.code === props.currencyToRemove)
+        .concat(props.currencies)
+    );
+    props.removeCurrency(null);
+  };
   return (
     <div className="Popup">
       <div className="Title">
         Do you want to remove ALL currencies from favorites?
       </div>
 
-      <Button type="Remove">Remove</Button>
+      <Button type="Remove" clicked={removeHandler}>
+        Remove
+      </Button>
       <Button>Cancel</Button>
     </div>
   );
 };
 
-export default Popup;
+const mapStateToProps = (state) => {
+  return {
+    currencies: state.currencies,
+    favorites: state.favoriteCurrencies,
+    currencyToRemove: state.currencyToRemove,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateFavorites: (favorites) => dispatch(action.updateFavorites(favorites)),
+    updateCurrencies: (currencies) =>
+      dispatch(action.updateCurrencies(currencies)),
+    removeCurrency: (currency) => dispatch(action.removeCurrency(currency)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Popup);
