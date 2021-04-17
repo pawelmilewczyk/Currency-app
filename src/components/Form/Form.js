5280641551e4c2771a6211c0b172b888f016eca1
pawelmilewczyk <milewczykp@gmail.com>
2021-04-17 07:45:31 +0200

    Add getData action to Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import "./Form.scss";
 import Button from "../../shared/Button/Button";
+import { connect } from "react-redux";
+import * as action from "../../store/actions";
 
 const Form = (props) => {
+  useEffect(() => {
+    props.getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <form className="Form">
       <h1 className="Title">Choose your favorites currencies</h1>
@@ -16,4 +24,16 @@ const Form = (props) => {
   );
 };
 
-export default Form;
+const mapStateToProps = (state) => {
+  return {
+    currencies: state.currencies,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getData: () => dispatch(action.getData()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
